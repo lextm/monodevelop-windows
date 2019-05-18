d53519da04c8f83cf086d22c63894e634324138a
Lex Li <support@lextm.com>
2019-05-18 18:51:39 -0400

    Moved monodoc.dll.

--- installer.bat
+++ installer.bat
@@ -1,5 +1,5 @@
 echo copy monodoc
-copy .\monodoc.dll .\monodevelop\setup\WixSetup\Libraries
+copy .\monodoc.dll .\monodevelop\main\build\bin
 @IF %ERRORLEVEL% NEQ 0 EXIT /b 1
 
 cd .\monodevelop\setup\WixSetup
