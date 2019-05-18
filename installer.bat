ad35610aa16504c13fde172372f9fa78d492e456
Lex Li <support@lextm.com>
2019-05-18 15:20:48 -0400

    Fixed installer.

--- installer.bat
+++ installer.bat
@@ -1,4 +1,4 @@
-copy .\monodevelop\main\external\mono-tools\windoc\WinDoc\lib\monodoc.dll .\monodevelop\setup\WixSetup\Libraries
+copy .\monodoc.dll .\monodevelop\setup\WixSetup\Libraries
 cd .\monodevelop\setup\WixSetup
 call build.bat
 copy .\bin\Release\Setup.msi ..\..\..
